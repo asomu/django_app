9379a9ba704e2a4bc82c5da01f51573056df0feb
Mark Jang <mark.jang@broadcom.com>
2024-09-12 10:14:22 +0900

    # INITIAL : Pybo site

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for mysite project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,13 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path
-from . import views
+from pybo import views
 
-app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id>/vote', views.vote, name='vote'),
+    path('admin/', admin.site.urls),
+    path('pybo/', views.index)
 ]
